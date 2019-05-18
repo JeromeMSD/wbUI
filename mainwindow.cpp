57bf3df7abdfa3fb3f54f3af5efcbec6f4d7c91c
Jerome MSD <jeromemsd@gmail.com>
2019-05-18 17:53:47 +0200

    Interface rapide

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,27 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    init();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::init(){
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer [80];
+
+    ui->state->setText("Disconnected");
+    // --- Date Set ---
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+
+    strftime (buffer,80,"%d - %m - %y",timeinfo);
+
+    ui->date->setText(buffer);
+
+
 }
